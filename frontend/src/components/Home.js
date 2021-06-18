c2b02d80ccd41e4b96f4f8fd1e75050b2304f523
ReverseStar <destro.mahmud@gmail.com>
2021-06-18 23:00:54 +0600

    Filter by Ratings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -21,6 +21,9 @@ const Home = ({ match }) => {
     const [currentPage, setCurrentPage] = useState(1)
     const [price, setPrice] = useState([1, 1000])
     const [category, setCategory] = useState('')
+    const [rating, setRating] = useState(0)
+
+
     const categories = [
         'Electronics',
         'Camera',
@@ -49,9 +52,9 @@ const Home = ({ match }) => {
             //alert.success('Success')
             return alert.error(error)
         }
-        dispatch(getProducts(keyword, currentPage, price, category))
+        dispatch(getProducts(keyword, currentPage, price, category, rating))
 
-    }, [dispatch, alert, error, keyword, currentPage, price, category])
+    }, [dispatch, alert, error, keyword, currentPage, price, category, rating])
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
@@ -96,7 +99,7 @@ const Home = ({ match }) => {
 
                                             <hr className="my-5"/>
                                             
-                                            <div className="my-5">
+                                            <div className="mt-5">
                                                 <h4 className="mb-3">
                                                     Categories
                                                 </h4>
@@ -110,6 +113,35 @@ const Home = ({ match }) => {
                                                             onClick={() => setCategory(category)}
                                                         >
                                                             {category}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                            {/* fknf3jtro34jto4jo4 */}
+                                            <hr className="my-3"/>
+                                            
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Ratings
+                                                </h4>
+
+                                                <ul className="pl-0">
+                                                    {[5,4,3,2,1].map(star => (
+                                                        <li
+                                                            style = {{cursor: 'pointer', listStyleType: 'none'
+                                                        }}
+                                                            key = {star}
+                                                            onClick={() => setRating(star)}
+                                                        >
+                                                            <div className="rating-outer">
+                                                                <div className="rating-inner"
+                                                                    style = {{
+                                                                        width: `${star * 20}%`
+                                                                    }}
+                                                                >
+                                                                    
+                                                                </div>
+                                                            </div>
                                                         </li>
                                                     ))}
                                                 </ul>
