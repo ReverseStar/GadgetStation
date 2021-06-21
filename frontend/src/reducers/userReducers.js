338e4bb8d911bb6a804e9ec9e7048f129a21be1f
ReverseStar <destro.mahmud@gmail.com>
2021-06-21 20:48:12 +0600

    F14M03

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,6 +2,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_FAIL,
     LOGIN_SUCCESS,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -9,11 +12,13 @@ export const authReducer = (state = { user: {} }, action) => {
     switch (action.type) {
 
         case LOGIN_REQUEST:
+        case REGISTER_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false
             }
         case LOGIN_SUCCESS:
+        case REGISTER_USER_REQUEST:
             return {
                 ...state,
                 loading: false,
@@ -21,6 +26,7 @@ export const authReducer = (state = { user: {} }, action) => {
                 user: action.payload
             }
         case LOGIN_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 ...state,
                 loading: false,
