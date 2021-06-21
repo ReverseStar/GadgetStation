1e6ff5267470435189976123fdc2947db7a7967f
ReverseStar <destro.mahmud@gmail.com>
2021-06-21 23:38:26 +0600

    F1406

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/layout/Header'
@@ -6,8 +7,15 @@ import Home from './components/Home'
 import ProductDetails from './components/product/ProductDetails'
 import Login from './components/user/Login'
 import Register from './components/user/Register'
+import {loadUser} from './actions/userActions'
+import store from './store'
 
 function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Router>
       <div className="App">
