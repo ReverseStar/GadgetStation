40302da1a5d2fc8d605f80425653eed64042b8a9
ReverseStar <destro.mahmud@gmail.com>
2021-06-22 15:50:39 +0600

    1413

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import store from './store'
 import Profile from './components/user/Profile'
 import ProtectedRoute from './components/route/ProtectedRoute'
 import UpdateProfile from './components/user/UpdateProfile'
+import UpdatePassword from './components/user/UpdatePassword'
 
 function App() {
 
@@ -31,6 +32,7 @@ function App() {
           <Route path="/register" component={Register} />
           <ProtectedRoute path="/me" component={Profile} exact />
           <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
+          <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
         </div>
         <Footer />
       </div>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,18 +3,28 @@ import {
     LOGIN_REQUEST,
     LOGIN_FAIL,
     LOGIN_SUCCESS,
+
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+
     LOAD_USER_REQUEST,
     LOAD_USER_SUCCESS,
     LOAD_USER_FAIL,
+
     UPDATE_PROFILE_REQUEST,
     UPDATE_PROFILE_SUCCESS,
     UPDATE_PROFILE_RESET,
     UPDATE_PROFILE_FAIL,
+
+    UPDATE_PASSWORD_REQUEST,
+    UPDATE_PASSWORD_SUCCESS,
+    UPDATE_PASSWORD_RESET,
+    UPDATE_PASSWORD_FAIL,
+
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
+
     CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -112,6 +122,32 @@ export const updateProfile = (userData) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: UPDATE_PROFILE_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+//Update Password
+export const updatePassword = (passwords) => async (dispatch) => {
+    try {
+        dispatch({type: UPDATE_PASSWORD_REQUEST})
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        
+        const {data} = await axios.put('/api/v1/password/update', passwords,config)
+        
+        dispatch({
+            type: UPDATE_PASSWORD_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: UPDATE_PASSWORD_FAIL,
             payload: error.response.data.message
         })
     }
